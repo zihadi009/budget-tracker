54abba1d312f74a221001280efe2a910ef70aa8b
Musfiqur Rahman Zihad <musfiqurahmanzihad@gmail.com>
2024-06-26 13:08:15 +0600

    done with the rose

--- valentaine-buddget-test-main/app.js
+++ valentaine-buddget-test-main/app.js
@@ -4,3 +4,12 @@ document.getElementById('kitkat-buy-btn').addEventListener('click',function(){
   const totalKitkatPrice = quantity * price;
   document.getElementById('chocolate').innerText = totalKitkatPrice;
 })
+
+document.getElementById('rose-buy-btn').addEventListener('click',function(){
+  const quantity = document.getElementById('rose-quantity').value;
+  const price = document.getElementById('rose-price').innerText;
+  const totalRosePrice = quantity * price;
+  document.getElementById('rose').innerText = totalRosePrice;
+})
+
+
