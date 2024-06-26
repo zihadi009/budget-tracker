e6dc7dfe73aba53a2fd703526a9200e8252595ab
Musfiqur Rahman Zihad <musfiqurahmanzihad@gmail.com>
2024-06-26 13:42:48 +0600

    completed the budget tracker with dom

--- valentaine-buddget-test-main/app.js
+++ valentaine-buddget-test-main/app.js
@@ -3,6 +3,7 @@ document.getElementById('kitkat-buy-btn').addEventListener('click', function () 
   const price = document.getElementById('kitkat-price').innerText;
   const totalKitkatPrice = quantity * price;
   document.getElementById('chocolate').innerText = totalKitkatPrice;
+  // document.getElementById('kitkat-quantity').value = ""
   total();
 })
 
@@ -11,6 +12,7 @@ document.getElementById('rose-buy-btn').addEventListener('click', function () {
   const price = document.getElementById('rose-price').innerText;
   const totalRosePrice = quantity * price;
   document.getElementById('rose').innerText = totalRosePrice;
+  // document.getElementById('rose-quantity').value = ""
   total()
 })
 
@@ -19,6 +21,7 @@ document.getElementById('diary-buy-btn').addEventListener('click', function () {
   const price = document.getElementById('diary-price').innerText;
   const totalDiaryPrice = quantity * price;
   document.getElementById('diary').innerText = totalDiaryPrice;
+  // document.getElementById('diary-quantity').value = ""
   total()
 })
 
@@ -28,11 +31,21 @@ function total() {
   const diary = document.getElementById('diary').innerText;
   const total = document.getElementById('total').innerText = parseInt(chocolate) + parseInt(rose) + parseInt(diary);
   const budget = document.getElementById('valentine-budget').innerText;
-  if(total > budget) {
-    alert("You are exceeding your budget.")
-  }
 }
 
-
+document.getElementById('apply-btn').addEventListener('click', function () {
+  const promoCode = document.getElementById('promo-code').value;
+  const total = document.getElementById('total').innerText;
+  const budget = document.getElementById('valentine-budget').innerText;
+  if (promoCode == 101) {
+    const newTotal = total - (total * 10 / 100)
+    if (newTotal > budget) {
+      alert("You're exceeding your budget.");
+    }
+    else {
+      document.getElementById('all-total').innerText = newTotal;
+    }
+  }
+})
 
 
